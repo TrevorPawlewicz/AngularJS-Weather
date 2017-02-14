b0c27337c84d794eaf0f49e287af80e5da9a476e
Trev <trevor.pawlewicz@gmail.com>
2017-02-14 09:43:39 -0500

    added Controllers and Roustes to pages

--- js/app.js
+++ js/app.js
@@ -1,2 +1,39 @@
-// module:
-var weatherApp = angular.module("weatherApp", ['ngRoute', 'ngResource']);
+// module:                       ng-app link
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+
+
+
+
+
+
+// CONTROLLERS:
+// 
+weatherApp.controller('homeController', ['$scope', function($scope){
+
+}]);
+
+//
+weatherApp.controller('forecastController', ['$scope', function($scope){
+
+}]);
+
+
+
+
+
+
+// ROUTES:
+weatherApp.config(function($routeProvider){
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        })
+});
