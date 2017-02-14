e1438538545a236e62db2ffc503b86089badfbbe
Trev <trevor.pawlewicz@gmail.com>
2017-02-14 10:25:28 -0500

    added a custom service for city input and display

--- js/app.js
+++ js/app.js
@@ -1,30 +1,10 @@
-// module:                       ng-app link
+// MODULE: where we write pieces of our Angular application.
+//                              ng-app link
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+//----------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-// CONTROLLERS:
-// 
-weatherApp.controller('homeController', ['$scope', function($scope){
-
-}]);
-
-//
-weatherApp.controller('forecastController', ['$scope', function($scope){
-
-}]);
-
-
-
-
-
-
-// ROUTES:
+// ROUTES: links to ng-view in index.html
 weatherApp.config(function($routeProvider){
 
     $routeProvider
@@ -37,3 +17,32 @@ weatherApp.config(function($routeProvider){
             controller: 'forecastController'
         })
 });
+//----------------------------------------------------------------------------
+
+// SERVICES: a Singleton object that will contain properties and Functions.
+weatherApp.service('cityService', function(){
+
+    this.city = "New York, NY"; // default
+});
+//----------------------------------------------------------------------------
+
+
+
+// CONTROLLERS: where we define our app's' behavior by defining Functions and Values.
+// for home.html
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+
+    $scope.city = cityService.city;
+
+    // watch triggers the Digest Loop "watching" for change of property
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });
+}]);
+
+// for forecast.html
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+
+    $scope.city = cityService.city;
+}]);
+//----------------------------------------------------------------------------
